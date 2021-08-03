667b54f4b7c1d3f3329c9605aeb97ef75b48d829
VikramKeswani <vikram.keswani108@gmail.com>
2021-08-03 13:11:09 +0530

    "second_commit_spring"

---
+++ CarBookingApplication/src/test/java/com/rupeek/CarBookingApplication/CarBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rupeek.CarBookingApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
